6650b5d81f022e1cb0b249e970471519a203a20c
JacksonPlayz <jackson.hardaway@gmail.com>
2020-10-28 18:34:49 -0700

    Implemented PlayerSizeEvent

--- src/main/java/com/minecraftabnormals/personality/core/event/PersonalityHooks.java
+++ src/main/java/com/minecraftabnormals/personality/core/event/PersonalityHooks.java
@@ -1,4 +1,15 @@
 package com.minecraftabnormals.personality.core.event;
 
+import net.minecraft.entity.EntitySize;
+import net.minecraft.entity.Pose;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraftforge.common.MinecraftForge;
+
 public class PersonalityHooks {
+
+    public static EntitySize getPlayerSize(EntitySize size, PlayerEntity entity, Pose pose) {
+        PlayerSizeEvent sizeEvent = new PlayerSizeEvent(entity, pose, size);
+        MinecraftForge.EVENT_BUS.post(sizeEvent);
+        return sizeEvent.getSize();
+    }
 }
---
+++ src/main/java/com/minecraftabnormals/personality/core/mixin/PlayerEntityMixin.java
@@ -0,0 +1,25 @@
+package com.minecraftabnormals.personality.core.mixin;
+
+import com.minecraftabnormals.personality.core.event.PersonalityHooks;
+import net.minecraft.entity.EntitySize;
+import net.minecraft.entity.Pose;
+import net.minecraft.entity.player.PlayerEntity;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import java.util.Map;
+
+@Mixin(PlayerEntity.class)
+public class PlayerEntityMixin {
+    @Shadow @Final private static Map<Pose, EntitySize> SIZE_BY_POSE;
+    @Shadow @Final public static EntitySize STANDING_SIZE;
+
+    @Inject(method = "getSize", at = @At("RETURN"))
+    public void fireSizeEvent(Pose pose, CallbackInfoReturnable<EntitySize> cir) {
+        cir.setReturnValue(PersonalityHooks.getPlayerSize(SIZE_BY_POSE.getOrDefault(pose, STANDING_SIZE), (PlayerEntity) (Object)this, pose));
+    }
+}
