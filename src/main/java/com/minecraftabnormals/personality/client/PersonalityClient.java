e463e357497461d877e14f5e6630fd79d6c7a42a
Jackson H <jackson.hardaway@gmail.com>
2021-03-01 09:53:12 -0800

    Now manually injecting into accessibility options. Fixes #13.
    Options are now also under sneak and sprint options like they are supposed to.

--- src/main/java/com/minecraftabnormals/personality/client/PersonalityClient.java
+++ src/main/java/com/minecraftabnormals/personality/client/PersonalityClient.java
@@ -10,61 +10,66 @@ import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.client.settings.KeyConflictContext;
 import net.minecraftforge.fml.client.registry.ClientRegistry;
 import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
-import org.apache.commons.lang3.ArrayUtils;
 import org.lwjgl.glfw.GLFW;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 
 public class PersonalityClient {
-	public static final KeyBinding CRAWL = new ToggleableKeyBinding("key.personality.crawl", GLFW.GLFW_KEY_C, "key.categories.gameplay", PersonalityConfig.CLIENT.keybinds.toggleCrawl::get);
-	public static final KeyBinding SIT = new ToggleableKeyBinding("key.personality.sit", GLFW.GLFW_KEY_Z, "key.categories.gameplay", PersonalityConfig.CLIENT.keybinds.toggleSitting::get);
+    public static final KeyBinding CRAWL = new ToggleableKeyBinding("key.personality.crawl", GLFW.GLFW_KEY_C, "key.categories.gameplay", PersonalityConfig.CLIENT.keybinds.toggleCrawl::get);
+    public static final KeyBinding SIT = new ToggleableKeyBinding("key.personality.sit", GLFW.GLFW_KEY_Z, "key.categories.gameplay", PersonalityConfig.CLIENT.keybinds.toggleSitting::get);
 
-	public static final IteratableOption CRAWL_OPTION = new IteratableOption(
-			PersonalityClient.CRAWL.getKeyDescription(),
-			(settings, value) -> PersonalityConfig.CLIENT.keybinds.toggleCrawl.set(!PersonalityConfig.CLIENT.keybinds.toggleCrawl.get()),
-			(settings, option) -> new TranslationTextComponent("options.generic_value", new TranslationTextComponent(PersonalityClient.CRAWL.getKeyDescription()), new TranslationTextComponent(PersonalityConfig.CLIENT.keybinds.toggleCrawl.get() ? "options.key.toggle" : "options.key.hold"))
-	);
-	public static final IteratableOption SIT_OPTION = new IteratableOption(
-			PersonalityClient.SIT.getKeyDescription(),
-			(settings, value) -> PersonalityConfig.CLIENT.keybinds.toggleSitting.set(!PersonalityConfig.CLIENT.keybinds.toggleSitting.get()),
-			(settings, option) -> new TranslationTextComponent("options.generic_value", new TranslationTextComponent(PersonalityClient.SIT.getKeyDescription()), new TranslationTextComponent(PersonalityConfig.CLIENT.keybinds.toggleSitting.get() ? "options.key.toggle" : "options.key.hold"))
-	);
+    public static final IteratableOption CRAWL_OPTION = new IteratableOption(
+            PersonalityClient.CRAWL.getKeyDescription(),
+            (settings, value) -> PersonalityConfig.CLIENT.keybinds.toggleCrawl.set(!PersonalityConfig.CLIENT.keybinds.toggleCrawl.get()),
+            (settings, option) -> new TranslationTextComponent("options.generic_value", new TranslationTextComponent(PersonalityClient.CRAWL.getKeyDescription()), new TranslationTextComponent(PersonalityConfig.CLIENT.keybinds.toggleCrawl.get() ? "options.key.toggle" : "options.key.hold"))
+    );
+    public static final IteratableOption SIT_OPTION = new IteratableOption(
+            PersonalityClient.SIT.getKeyDescription(),
+            (settings, value) -> PersonalityConfig.CLIENT.keybinds.toggleSitting.set(!PersonalityConfig.CLIENT.keybinds.toggleSitting.get()),
+            (settings, option) -> new TranslationTextComponent("options.generic_value", new TranslationTextComponent(PersonalityClient.SIT.getKeyDescription()), new TranslationTextComponent(PersonalityConfig.CLIENT.keybinds.toggleSitting.get() ? "options.key.toggle" : "options.key.hold"))
+    );
 
-	static {
-		addAccessibilityOptions();
-	}
+    static {
+        addAccessibilityOptions();
+    }
 
-	public static void registerKeyBinds() {
-		CRAWL.setKeyConflictContext(KeyConflictContext.IN_GAME);
-		SIT.setKeyConflictContext(KeyConflictContext.IN_GAME);
-		ClientRegistry.registerKeyBinding(CRAWL);
-		ClientRegistry.registerKeyBinding(SIT);
-	}
+    public static void registerKeyBinds() {
+        CRAWL.setKeyConflictContext(KeyConflictContext.IN_GAME);
+        SIT.setKeyConflictContext(KeyConflictContext.IN_GAME);
+        ClientRegistry.registerKeyBinding(CRAWL);
+        ClientRegistry.registerKeyBinding(SIT);
+    }
 
-	private static void addAccessibilityOptions() {
-		try {
-			Field optionsField = ObfuscationReflectionHelper.findField(AccessibilityScreen.class, "field_212986_a");
-			Field modifiedField = Field.class.getDeclaredField("modifiers");
-			modifiedField.setAccessible(true);
-			modifiedField.setInt(optionsField, optionsField.getModifiers() & ~Modifier.FINAL);
+    private static void addAccessibilityOptions() {
+        try {
+            Field optionsField = ObfuscationReflectionHelper.findField(AccessibilityScreen.class, "field_212986_a");
+            Field modifiedField = Field.class.getDeclaredField("modifiers");
+            modifiedField.setAccessible(true);
+            modifiedField.setInt(optionsField, optionsField.getModifiers() & ~Modifier.FINAL);
 
-			AbstractOption[] options = (AbstractOption[]) optionsField.get(null);
+            AbstractOption[] options = (AbstractOption[]) optionsField.get(null);
 
-			if (options == null)
-				throw new NullPointerException("Accessibility options were null.");
+            if (options == null)
+                throw new NullPointerException("Accessibility options cannot be null");
 
-			optionsField.set(null, addButtons(options, 10, PersonalityClient.CRAWL_OPTION, PersonalityClient.SIT_OPTION));
-		} catch (Exception e) {
-			throw new RuntimeException("Failed to add accessibility option.", e);
-		}
-	}
+            optionsField.set(null, addButtons(options, 10, PersonalityClient.CRAWL_OPTION, PersonalityClient.SIT_OPTION));
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to add accessibility options", e);
+        }
+    }
 
-	private static AbstractOption[] addButtons(AbstractOption[] src, int startingIndex, AbstractOption... options) {
-		int i = startingIndex;
-		// TODO: Fix array inserting
-		for (AbstractOption option : options)
-			src = ArrayUtils.add(src, option);
-		return src;
-	}
+    private static AbstractOption[] addButtons(AbstractOption[] src, int startingIndex, AbstractOption... options) {
+        int index = startingIndex;
+        for (AbstractOption option : options) {
+            AbstractOption[] tmp = new AbstractOption[src.length + 1];
+
+            System.arraycopy(src, 0, tmp, 0, index);
+            tmp[index] = option;
+            System.arraycopy(src, index, tmp, index + 1, src.length - index);
+            src = tmp;
+            index++;
+        }
+        return src;
+    }
 }
